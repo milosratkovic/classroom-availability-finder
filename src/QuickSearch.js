7c31869b76e19045316b1b31949d3ac1f42123a7
Milos Ratkovic <43796515+milosratkovic@users.noreply.github.com>
2023-04-02 16:56:59 -0700

    Update QuickSearch.js

--- src/QuickSearch.js
+++ src/QuickSearch.js
@@ -112,6 +112,18 @@ return ` for ${dayOfWeek} ${dateSelected} from ${start} to ${end}`
         
 
       </form>
+  <iframe
+  src="//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=primestudent&banner=09T4RTRGDTMCPCMYJVR2&f=ifr&linkID=e1c605fe9f198762a1ff5a6ea07340b6&t=milos05-20&tracking_id=milos05-20"
+  width="300"
+  height="250"
+  scrolling="no"
+  border="0"
+  marginWidth="0"
+  style={{border: 'none'}}
+  frameBorder="0"
+  sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"
+/>
+
         <SearchResults resultInfo = {resultInfo} availableClassrooms = {availableClassrooms} />
         
 
