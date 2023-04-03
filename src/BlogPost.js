5307de43e0f86546f1919a840c43a3f3c1d8c9e1
Milos Ratkovic <43796515+milosratkovic@users.noreply.github.com>
2023-04-02 18:08:29 -0700

    Update BlogPost.js

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -4,6 +4,28 @@ import './BlogPost.css'; // Import CSS file for styling
 const BlogPost = () => {
   return (
     <div className="blog-post">
+      <h2 className="blog-post-title">Make Your Student Life Easier with Amazon Prime Student</h2>
+      <p>As a student at Simon Fraser University, you can benefit greatly from the many perks that come with an Amazon Prime Student membership. With free two-day shipping on eligible items, you can get all the textbooks and supplies you need without having to leave campus or wait for days for them to arrive. Plus, with Prime Video and Prime Music streaming, you can unwind after a long day of classes and studying by watching your favorite TV shows, movies, and listening to music.</p>
+      <p>But that's not all. Amazon Prime Student also offers unlimited photo storage, so you never have to worry about running out of space on your phone or laptop. And with exclusive deals and discounts just for students, you can save money while still getting everything you need.</p>
+      <p>The best part? You can try Amazon Prime Student for free for six months, and then pay only 50% of the regular membership fee once the trial ends. And if you sign up using our affiliate link, you'll not only get all the benefits of Amazon Prime Student, but you'll also be supporting our website.</p>
+      <p>So what are you waiting for? Join the millions of students already enjoying the many perks of Amazon Prime Student, and make your life a little easier and more convenient today.</p>
+    <center><iframe 
+  src="//ws-na.amazon-adsystem.com/widgets/cm?o=15&p=12&l=ur1&category=primestudent&banner=09T4RTRGDTMCPCMYJVR2&f=ifr&linkID=e1c605fe9f198762a1ff5a6ea07340b6&t=milos05-20&tracking_id=milos05-20"
+  width="300"
+  height="250"
+  scrolling="no"
+  border="0"
+  marginwidth="0"
+  style={{border: 'none'}}
+  frameborder="0"
+  sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"
+></iframe>
+ </center>
+    </div>
+    
+    
+    
+    <div className="blog-post">
       <h2 className="blog-post-title">10 Tips for Finding the Best Study Spaces and Boosting Your Productivity at Simon Fraser University</h2>
       <ol className="blog-post-list">
         <li className="blog-post-item">Use Classroomfinder.ca: Classroomfinder.ca is a website that lets you input a time range and date to search for an empty classroom at Simon Fraser University that you can use to study in. This is a great tool to help you find available study spaces on campus.</li>
